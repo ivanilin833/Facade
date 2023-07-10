f9b432ac6ac30d8836d976af4804e359332cacae
Ivan Ilin <ilyin1997@icloud.com>
2023-07-10 21:06:33 +0300

    add project

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(BinOps.sum("100", "101"));
+        System.out.println(BinOps.mult("111", "0"));
+    }
+}
